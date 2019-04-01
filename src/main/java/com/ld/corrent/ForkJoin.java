ee63a723ddd76544a2bbb2ff4c26d81334eb4260
dzfking007 <dzfking007@163.com>
2019-04-01 17:34:10 +0800

    create

---
+++ src/main/java/com/ld/corrent/ForkJoin.java
@@ -0,0 +1,15 @@
+package com.ld.corrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Description
+ * @Author dzfking007 (<a href="mailto:dzfking007@163.com">dzfking007@163.com</a>)
+ * 2019/4/1 17:24
+ */
+@Slf4j
+public class ForkJoin {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
